a7a78baed2687f1f5afb782859056d8143111db8
Omar Albeik <omaralbeik@gmail.com>
2016-03-11 13:25:30 +0200

    feat: database and tables created

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,52 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM matches;")
+    db.commit()
+    db.close()
+    print "____________________________________________"
+    print "=> All matches were deleted successfully."
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("DELETE FROM players;")
+    db.commit()
+    db.close()
+    print "____________________________________________"
+    print "=> All players were deleted successfully."
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("SELECT COUNT(id) FROM players;")
+    rows = c.fetchall()
+    db.close()
+    return rows[0][0]
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s)",(name,))
+    db.commit()
+    db.close()
+    print "____________________________________________"
+    print "=> player " + name + "were created and added to database successfully."
 
 
 def playerStandings():
@@ -49,6 +76,7 @@ def playerStandings():
     """
 
 
+
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
 
@@ -56,16 +84,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +101,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,29 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- remove any old database (if available)
+DROP DATABASE IF EXISTS tournament;
 
+-- drop any old tables
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+
+-- create the database
+CREATE DATABASE tournament;
+
+-- players table
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+)
+
+-- matches table
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES players (id) ON DELETE CASCADE,
+  opponent_id INTEGER REFERENCES players (id) ON DELETE CASCADE,
+  winner_id INTEGER REFERENCES players (id) ON DELETE CASCADE
+)
+
+-- connect to database
+\c tournament;
